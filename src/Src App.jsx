d4788bf80420dbfab10fd89edacf01dc35280dd2
Amanda Yvette <amandayhayes1@gmail.com>
2025-07-29 03:41:55 -0400

    Create Src App.jsx

---
+++ src/Src App.jsx
@@ -0,0 +1,11 @@
+import WordWiggleGame from "./components/WordWiggleGame";
+
+function App() {
+  return (
+    <div className="min-h-screen bg-white flex items-center justify-center">
+      <WordWiggleGame />
+    </div>
+  );
+}
+
+export default App;
